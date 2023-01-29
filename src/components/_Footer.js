c62dfd178f8b79f2bf77d21a3ada939d4b6716a5
darioaladuz <baladuz98@gmail.com>
2023-01-29 15:45:59 +0100

    add linkedin url

--- src/components/_Footer.js
+++ src/components/_Footer.js
@@ -1,5 +1,5 @@
 export default function footer() {
     const footer = document.createElement('footer');
-    footer.innerHTML = 'Created by: <a href="#">Dario Aladuz</a>';
+    footer.innerHTML = 'Created by: <a href="https://www.linkedin.com/in/dario-aladuz/">Dario Aladuz</a>';
     return footer;
 }
